c17172d98ff802f7817fe396e25bc4aae68e57ab
Eric <eric.kong98@gmail.com>
2015-09-05 18:32:10 -0400

    Skeet Skeet Skeet

--- application/mod_game/controllers.py
+++ application/mod_game/controllers.py
@@ -1,1 +1,10 @@
 from flask import Flask, render_template, Blueprint
+from application.mod_api.controllers import *
+
+import json
+
+def getSectors():
+    data = get_sectors();
+    sectors = json.load(data);
+    return render_template('index.html',
+                            sectors = sectors);
