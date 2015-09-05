87a7271e0cab02b3bbffa0a4d545cac27e9faa37
Robert Kim <rkty13@gmail.com>
2015-09-05 18:34:14 -0400

    graph now renders on d3-panel

--- application/static/js/visualize.js
+++ application/static/js/visualize.js
@@ -5,7 +5,7 @@ var StockGraph = function(height, width, margins, data) {
   this.margins = margins;
   this.data = data;
   this.maximum = d3.max(this.data, function(d) {
-    return parseFloat(d.adj_close_p);
+    return parseFloat(d.close_p);
   });
   console.log(this.maximum);
 
@@ -23,7 +23,7 @@ var StockGraph = function(height, width, margins, data) {
 
   this.draw = function() {
     var that = this;
-    var vis = d3.select('#visualization')
+    var vis = d3.select(".d3-panel")
         .attr("width", this.width + this.margins.left + this.margins.right)
         .attr("height", this.height + this.margins.top + this.margins.bottom),
       xRange = d3.time.scale()
@@ -51,7 +51,7 @@ var StockGraph = function(height, width, margins, data) {
 
     var lineGen = d3.svg.line()
       .x(function (d) { return xRange(that.toDate(d.date_ex)); })
-      .y(function (d) { return yRange(parseFloat(d.adj_close_p)); })
+      .y(function (d) { return yRange(parseFloat(d.close_p)); })
     vis.append("path")
       .attr("d", lineGen(data))
       .attr("stroke-width", 2)
