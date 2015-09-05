6037b471df2789be261fbe99afdcc2bcd5651bf3
Eric <eric.kong98@gmail.com>
2015-09-05 18:36:58 -0400

    JOHN CENA

--- application/mod_game/controllers.py
+++ application/mod_game/controllers.py
@@ -3,6 +3,8 @@ from application.mod_api.controllers import *
 
 import json
 
+mod_game = Blueprint('game', __name__, url_prefix='/game')
+
 def getSectors():
     data = get_sectors();
     sectors = json.load(data);
